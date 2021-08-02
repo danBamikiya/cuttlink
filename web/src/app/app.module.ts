24591b39200881c428499bf94b5c2581233ec5a3
Dan Bamikiya <dudeawesome732@gmail.com>
2021-08-02 23:46:01 +0000

    style: formatter changes :art:

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,19 +1,25 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { MatIconModule } from '@angular/material/icon';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { HttpClientModule } from '@angular/common/http'
+import { MatIconModule } from '@angular/material/icon'
+import { FormsModule } from '@angular/forms'
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/layout/header/header.component';
-import { ShortenerComponent } from './components/shortener/shortener.component';
+import { AppComponent } from './app.component'
+import { HeaderComponent } from './components/layout/header/header.component'
+import { ShortenerComponent } from './components/shortener/shortener.component'
 
-import { ClipboardModule } from 'ngx-clipboard';
-import { HttpClientModule } from '@angular/common/http';
+import { ClipboardModule } from 'ngx-clipboard'
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, ShortenerComponent],
-  imports: [BrowserModule, MatIconModule, FormsModule, ClipboardModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    MatIconModule,
+    FormsModule,
+    ClipboardModule,
+    HttpClientModule
+  ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}

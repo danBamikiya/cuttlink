ffd669d50f68b450666c4334a0fa19e69c7a227f
Dan Bamikiya <dudeawesome732@gmail.com>
2021-08-03 00:01:35 +0000

    feat: add api url to environments :adhesive_bandage:

--- web/src/environments/environment.ts
+++ web/src/environments/environment.ts
@@ -3,8 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
-};
+  production: false,
+  API_URL: 'https://3000-magenta-camel-dcys2luy.ws-eu13.gitpod.io/api/url'
+}
 
 /*
  * For easier debugging in development mode, you can import the following file

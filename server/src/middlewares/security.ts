54e697077a86bbb494fd905f9988e508abd07c3c
Dan Bamikiya <dudeawesome732@gmail.com>
2021-08-17 01:47:58 +0000

    feat: use redis to persit rate limit attempts :card_file_box:
    * reduce the timeframe for requests from 10 mins to 3 mins
    * use custom 429 HTTP error handler for when max limit is reached

--- server/src/middlewares/security.ts
+++ server/src/middlewares/security.ts
@@ -1,12 +1,33 @@
 import express, { Router } from 'express'
 import rateLimit from 'express-rate-limit'
+import RedisStore from 'rate-limit-redis'
 import helmet from 'helmet'
+import { redisClient } from '../config/cache'
+import { HTTP429Error } from '../utils/httpErrors'
+
+const isProduction = process.env.NODE_ENV === 'production'
+const EXPIRES_IN_AS_SECONDS = 3 * 60
 
 const handleRateLimit = (router: Router) => {
   const limit = rateLimit({
+    // 3 mins in `ms` (or practically unlimited outside production)
+    windowMs: isProduction ? EXPIRES_IN_AS_SECONDS * 1000 : 1,
     max: 100, // limit each IP to 100 requests per windowMs
-    windowMs: 30 * 60 * 1000, // 30 mins, the timeframe for which requests are checked
-    message: 'Too many requests.'
+    handler: () => {
+      throw new HTTP429Error(
+        'Too many requests from this IP, please try again after 3 minutes.'
+      )
+    },
+    // the storage to use when persisting rate limit attempts
+    store: new RedisStore({
+      client: redisClient,
+      // 3 mins in `s` (or practically unlimited outside production)
+      expiry: isProduction ? EXPIRES_IN_AS_SECONDS : 1,
+      prefix: 'rl:',
+      // @ts-ignore
+      // If Redis is not connected, let the request succeed as failover
+      passIfNotConnected: true
+    })
   })
 
   router.use(limit)
